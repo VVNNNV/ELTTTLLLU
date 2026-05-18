11b708df64fd771846ec0dedfd1e5973079aad42
VVNNNV <117117030+VVNNNV@users.noreply.github.com>
2026-05-18 17:40:27 +0800

    elt

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,30 +1,28 @@
-import { Typography } from "antd";
 import logoImg from "/logo-new.png";
 import style from "./index.module.scss";
 import { observer } from "mobx-react-lite";
 
 interface LogoProps {
-  iconSize?: number;
   title?: string;
   showImage?: boolean;
 }
 
 export const Logo = observer(({ title = "PicUn tu", showImage = true }: LogoProps) => {
   return (
-    <div
-      className={style.container}
-      onClick={() => window.open("https://tttia.com/", "_blank")}
+    <div 
+      className={style.container} 
+      onClick={() => window.open("https://tttai.eu.cc", "_blank")}
       style={{ cursor: "pointer" }}
     >
       {showImage && (
-        <img
-          src={logoImg}
-          alt="PicUn tu Logo"
+        <img 
+          src={logoImg} 
+          alt="PicUn tu Logo" 
           className={style.logoImage}
           title={title}
         />
       )}
-      <Typography.Text style={{ fontWeight: 'bold', fontSize: '18px' }}>{title}</Typography.Text>
+      <span>{title}</span>
     </div>
   );
 });
